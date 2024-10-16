d9b10e8b116bcc92592afab4830b60ef1bd4c28d
toel-kamanda34 <toelokemwa@gmail.com>
2024-10-16 20:34:00 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -52,6 +52,41 @@ api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
 
+class Login(Resource):
+
+    def post(self):
+
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+
+        session['user_id'] = user.id
+
+        return user.to_dict(),200
+    
+api.add_resource(Login, '/login')
+
+class Logout(Resource):
+    def delete(self):
+
+        session['user_id'] = None
+
+        return { },204
+    
+api.add_resource(Logout, '/logout')
+
+class CheckSession(Resource):
+    def get(self):
+
+        user_id = session.get('user_id')
+
+        if user_id:
+
+            user = User.query.filter(User.id == user_id).first()
+
+            return user.to_dict(),200
+        
+        return{},401
+    
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
